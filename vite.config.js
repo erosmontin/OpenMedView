b15337672b55d34674ad20e9733564a06ce50ebb
Eros <eros.montin@gmail.com>
2025-08-01 15:26:01 -0400

    chrom release 0.1.3

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'OpenMedView',
+      formats: ['es', 'cjs'],
+      fileName: (format) => `index.${format}.js`
+    },
+    rollupOptions: {
+      // don’t bundle peer deps
+      external: ['react', 'react-dom', '@niivue/niivue'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          '@niivue/niivue': 'Niivue'
+        }
+      }
+    }
+  }
 })
