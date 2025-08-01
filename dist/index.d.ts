ea959cd6345c70c168179f655aa55605399d469b
Eros <eros.montin@gmail.com>
2025-08-01 15:13:48 -0400

    chore(release): 0.1.1

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+export { default as OpenMedView } from './components/OpenMedView';
+export type { OpenMedViewProps } from './components/OpenMedView';
+//# sourceMappingURL=index.d.ts.map
---
+++ dist/index.js
@@ -0,0 +1,2 @@
+export { default as OpenMedView } from './components/OpenMedView';
+//# sourceMappingURL=index.js.map
